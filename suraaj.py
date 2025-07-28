3041abe87e05746d1384f46c35765e1ab928c561
CHANDAN KUMAR <chandanmanish.007@gmail.com>
2025-07-28 23:21:42 +0530

    new updates

--- suraaj.py
+++ suraaj.py
@@ -7,4 +7,4 @@ name = input("What is your name? ")
 print("Hello, " + name + "! Nice to meet you, July 25 Batch.")
 
 print("ADD TO CART!")
-print("Your Item has been Removed! Sorry :) ;) Chandan This side")
+print("Your Item has been Removed! Sorry :) ;) ")
